3e2066472ea7df7235d7e8e81d0162d8ae7e2858
shin.unuma <>
2024-11-13 23:23:08 +0900

    Xcodeプロジェクト追加

---
+++ graphql-swift-sample/graphql_swift_sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  graphql_swift_sampleApp.swift
+//  graphql-swift-sample
+//
+//  Created by Shin Unuma on 2024/11/13.
+//
+
+import SwiftUI
+
+@main
+struct graphql_swift_sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ graphql-swift-sampleTests/graphql_swift_sampleTests.swift
@@ -0,0 +1,16 @@
+//
+//  graphql_swift_sampleTests.swift
+//  graphql-swift-sampleTests
+//
+//  Created by Shin Unuma on 2024/11/13.
+//
+
+import Testing
+
+struct graphql_swift_sampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
